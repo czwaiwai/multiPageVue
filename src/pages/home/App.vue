44d30aa0cd9e6e3af32165c0ad5c9eab12b8317b
caozheng <caozheng@syswin.com>
2019-03-21 09:59:09 +0800

    多页基础模版

---
+++ src/pages/home/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    这个是测试页面---
+    为什么始终去的是test目录的页面
+    哈哈哈啊----
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
